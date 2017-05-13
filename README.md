749cec85518d48efac30178cffe79bcce6e09ba7
Pat <p@pho.sx>
2017-05-13 21:17:14 +1200

    Improves regexes. Reduces strictness for bigram matching and other heuristics

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-A JS port of [academia-edu/dejunk](https://github.com/academia-edu/dejunk).
+A modified JS port of [academia-edu/dejunk](https://github.com/academia-edu/dejunk).
+
+Detect keyboard mashing and other junk in your data.
+
+Uses a variety of heuristics, the most sophisticated being a comparison of bigrams in the input to the frequencies in a "known-good" corpus vs. their proximity on a keyboard. Achieves pretty good precision on Academia.edu's data, but might need adjustment for yours.
 
 
 ``` javascript
@@ -7,6 +11,8 @@ var isJunk = require("dejunk.js").isJunk
 
 isJunk('asd asd das as d'); // true
 
+
+// if you need more info pass true as second parameter
 var returnString = true;
 
 isJunk('asd asd das as d', returnString); // "asdf_row"
--- dejunk.js
+++ dejunk.js
@@ -1,20 +1,26 @@
 const zip = require('lodash.zip');
 
 const arrFlatten = (arr) => [].concat.apply([], arr);
-
 const strReverse = (str) => str.split('').reverse().join('');
 
 // All characters on the middle row of a QWERTY keyboard
 const MASH_CHARS = 'ASDFGHJKLasdfghjkl;: ';
-// CURRENT -> trying to make this logic return the same result.
 const  MASH_BIGRAMS = _mashBigrams();
-
 
 // TODo implement better meomoization. these aren't always needed.
 const corpusBigramFrequencies  = require('./bigram_frequencies.json');
 const mashingBigramFrequencies = _mashingBigramFrequencies();
 const corpusBigramMagnitude  = _corpusBigramMagnitude();
 const mashingBigramMagnitude = _mashingBigramMagnitude();
+
+/*
+  TODO: if mosts words in a sentence are good we should allow it.
+  So. i isJunk == true return the following:
+  const words = string.split(' ')
+  if (words.length > 5) {
+    return (words.filter(junk).length / words.length) > 0.85
+  }
+*/
 
 function isJunk(inputString, returnStr=false, minAlnumChars=3, whitelistRegexes=[], whiteliststrs=[]) {
 
@@ -31,7 +37,7 @@ function isJunk(inputString, returnStr=false, minAlnumChars=3, whitelistRegexes=
     return false;
   }
 
-  if (!str || /\w+/.test(str) === false) {
+  if (!str || /[A-z]+/.test(str) === false) {
     return false; //'not_alphanumeric'
   }
 
@@ -40,25 +46,27 @@ function isJunk(inputString, returnStr=false, minAlnumChars=3, whitelistRegexes=
     return false; // email_address
   }
 
-  if (normed.length < minAlnumChars) return returnVal('too_short', true);
+  if(/[A-Z]{2,8}/.test(str)) {
+    return false; // acronym
+  }
+
   if (excessiveSingleCharacterRepeats(str, normed)) return returnVal('one_char_repeat', true);
-  if (startsWithDisallowedPunctuation(str)) return returnVal('starts_with_punct', true);
+  if (startsWithMultiplePunctuation(str)) return returnVal('starts_with_punct', true);
   if (tooManyShortWords(str)) return returnVal('too_many_short_words', true);
   if (threePlusCharsRepeatTwice(str)) return returnVal('three_chars_repeat_twice', true);
   if (missingVowels(str, normed)) return returnVal('missing_vowels', true);
   if (asdfRowAndSuspicious(str)) return returnVal('asdf_row', true);
 
-  const asciiProportion = [...str].filter(c => c.charCodeAt() < 128).length / str.length
+  const asciiProportion = [...str].filter(c => c.charCodeAt() < 128).length / str.length;
 
   // The bigrams look like the ones you'd get from keyboard mashing (the probability shouldn't be
   // taken too literally, > 0.25 is almost all mashing in practice on our corpus)
   if (str.length > 1 && asciiProportion > 0.8) {
-    if (probabilityOfKeyboardMashing(str) > 0.25){
+    if (probabilityOfKeyboardMashing(str) > 0.99){ // bumped up from 0.25
       return returnVal('mashing_bigrams', true);
     }
   }
 
-  // The bigrams don't look like the bigrams in legitimate strs
   if (str.length > 6 && asciiProportion > 0.8) {
 
       const corpus_similarity = bigramSimilarityToCorpus(str);
@@ -67,17 +75,15 @@ function isJunk(inputString, returnStr=false, minAlnumChars=3, whitelistRegexes=
       // so increase the value (= lower the threshold) for shorter strs and strs with less ASCII.
       const score = corpus_similarity * (1.0/asciiProportion*asciiProportion) * (1.0/(1 - Math.exp(-0.1*str.length)))
 
-      if (score < 0.03) {
+      // The similarity ignores casing, so instead use a higher threshold if the casing looks wrong
+      if (score < 0.01 || score < 0.04 && ! /^([A-Z][a-z]+ )*[A-Z][a-z]+$/.test(str)) { // reduced sensitivity
         return returnVal('unlikely_bigrams', true);
-      }
-      else if (score < 0.08 && ! /^([A-Z][a-z]+ )*[A-Z][a-z]+$/.test(str)) {
-        // The similarity ignores casing, so instead use a higher threshold if the casing looks wrong
-        return returnVal('unlikely_bigrams', true);
-      }
-      else if (score < bigramSimilarityToMashing(str)) {
-        return returnVal('mashing_bigrams', true);
       }
 
+      else if (bigramSimilarityToMashing(str) - score > 0.05) {
+      // else if (score < bigramSimilarityToMashing(str)) {
+        return returnVal('mashing_bigrams', true);
+      }
   }
 
   return false
@@ -96,53 +102,25 @@ function _mashBigrams() {
 }
 
 function normalizeForComparison(str) {
-  return str.normalize('NFD')
+    return str.normalize('NFD')
             .replace(/[\u0300-\u036f]/g, "")
             .replace(/[^\w\d]/g, "")
             .toLowerCase();
 }
 
-
-// One character repeated 5 or more times, or 3 or more times and not an
-// acronym, roman numeral, or www
+// One non numeric character repeated 5 or more times
 function excessiveSingleCharacterRepeats(str, normed) {
-
-    // if there's only one letter in the str
-    if ([...new Set(normed)].length == 1) {
-      return true
-    }
-
-    if (/([^\s])\1\1/i.test(str)) {
-      if (/([^0-9])\1\1\1\1/i.test(normed)) {
-        return true
-      }
-
-      const punct = /[\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/;
-      const words = str.split(punct);
-
-      for(let i=0; i < words.length; i++) {
-        if (/([^iw0-9])\1\1/i.test(words[i]) && words[i] !== words[i].toUpperCase()) {
-          return true;
-        }
-      }
-    }
-
-    return false
+    return /(\D)\1{4,}/g.test(str)
 }
 
-
-
-// Starting punctuation, except ['"#@]
-// Ruby Original: str =~ /\A[[:punct:]]/ && str !~ /\A(\p{Pi}|\p{Ps}|['"¿»’]).+/
-function startsWithDisallowedPunctuation(str) {
-  const startsWithPunct = /^[\s\u2000-\u206F\u2E00-\u2E7F\\'!"$%&()*+,\-.\/:;<=>?\[\]^_`{|}~]/
-  return startsWithPunct.test(str)
+function startsWithMultiplePunctuation(str) {
+  return /^([\s\$%&()*+,\-\/:;<=>?\[\]^_{|}~!'"@#¿¡`]{3,}|[.]{4,})/.test(str)
 }
-
 
 function tooManyShortWords(str) {
-
   const words = str.split(' ')
+  if (words.length < 5) { return false; }
+
   const short = words.filter( (w) => w.length < 3 ).length
 
   if (short > 2 && short > 0.75 * words.length) {
@@ -155,22 +133,20 @@ function tooManyShortWords(str) {
 // At least 3 characters repeated at least twice in a row (but only on short
 // strs, otherwise there are false positives)
 function threePlusCharsRepeatTwice(str) {
-  // ruby: /(....*)[[:space:][:punct:]]*\1[[:space:][:punct:]]*\1/
-  const regexp = /(....*)[\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]*\1[\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]*\1/
-
-  return (str.length < 80 && regexp.test(str))
+  return (str.length < 80 && /(....*)[ \-._!@]*\1[ \-._!@]*\1/g.test(str))
 }
-
 
 // Missing vowels (and doesn't look like acronym, and is ASCII so we can tell)
 function missingVowels(str, normed) {
-    if (! [...normed].some(c => c.charCodeAt() >= 128) || str == str.toUpperCase() ) {
-      if (!/[aeiouy]/i.test(normed)) {
-        return true
-      }
+    const containsNonAscii = [...normed].some(c => c.charCodeAt() >= 128);
+    const isAcronym = str === str.toUpperCase();
+    const isNth = /^\d+(?:st|nd|rd|th|)$/.test(str);
+
+    if (containsNonAscii || isAcronym || isNth) {
+      return false;
     }
 
-    return false
+    return !/[aeiouy]/i.test(normed);
 }
 
 
@@ -222,23 +198,12 @@ function _bigrams(str) {
   return zip(chars, chars.slice(1))
            .filter(x => !!x[0] && !!x[1])
            .map(arr => arr.join('').toLowerCase())
-           .map(bigram => bigram.replace(/[\d]/g, '0')) // [ruby]  .gsub(/[0-9]/, '0'.freeze) not sure why it needs immutable '0'
-            // map { |bigram| bigram.gsub(/[[:space:]]/, ' '.freeze) } // [ruby]. not sure why replace space
+           .map(bigram => bigram.replace(/[\d]/g, '0'))
 }
 
 function mashingProbability(bigram) {
   const f = mashingBigramFrequencies[bigram];
-  if (f) {
-    return f
-  }
-  else if (/[a-z]{2}/i.test(bigram)) {
-    // 26**2 = 676, so 1 in 2k seems a reasonable probability for an arbitrary two-letter bigram given mashing
-    return 1e-6; //0.0005; ... bug from original. makes tests pass. need to fix.
-  }
-  else {
-    // An arbitrary (non-ASCII) bigram with mashing is slightly more probable than with legit strs
-    return 1e-6;
-  }
+  return f || 1e-6;
 }
 
 
@@ -256,6 +221,7 @@ function  _mashingBigramFrequencies() {
     });
     return mBF;
 }
+
 
 function corpusProbability(bigram) {
 
@@ -293,14 +259,10 @@ function bigramSimilarityToCorpus(str) {
 }
 
 
-
 function _corpusBigramMagnitude() {
 
   return objMagnitude(corpusBigramFrequencies)
 }
-
-
-
 
 
 // Cosine similarity between vector of frequencies of bigrams within str,
@@ -320,7 +282,6 @@ function bigramSimilarityToMashing(str) {
     return r;
   }, {});
 
-
   const numerator = Object.keys(freqs).map(bigram => {
     return (mashingBigramFrequencies[bigram] || 0) * freqs[bigram]
   }).reduce((a, b) => a + b);
@@ -330,17 +291,10 @@ function bigramSimilarityToMashing(str) {
   return numerator / denominator;
 }
 
-
 function _mashingBigramMagnitude() {
   return objMagnitude(mashingBigramFrequencies);
 }
 
-
 module.exports = {
   isJunk: isJunk
 }
-
-// let x = 'hadsjk hk∆j hadsjk sjh7kjé ah-sd';
-// console.log(missingVowels(x, normalizeForComparison(x)));
-
-isJunk('Unknown');
